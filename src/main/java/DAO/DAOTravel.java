36349b5eb5f313a9d6543b5a705d07e71b6b8bda
José Paz <jpaz17@hotmail.com>
2019-10-31 00:27:42 -0300

    Agregado Travel + consultas

---
+++ src/main/java/DAO/DAOTravel.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.Travel;
+import java.util.List;
+import java.sql.*;
+import DB.ConnectionFactory;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author estudiante.fit
+ */
+public class DAOTravel implements DAOInterface<Travel> {
+
+    private Travel extract(ResultSet rs) throws SQLException {
+        Travel t = new Travel();
+                
+        t.setCi_driver(rs.getInt("ci_driver"));
+        t.setCi_passenger(rs.getInt("ci_passenger"));
+        t.setDate_ini(rs.getString("date_ini"));
+        t.setDate_end(rs.getString("date_end"));
+        t.setMatricula_vehicle(rs.getString("matricula_vehicle"));
+        t.setSeats_available(rs.getInt("seats_avalaible"));
+        t.setCi_driver(rs.getInt("companions"));
+        
+        return t;
+    }
+    
+    @Override
+    public Travel get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Travel> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public List<Travel> getAllFromUser(int ci) {
+        Connection con = ConnectionFactory.getConnection();
+        String query = String.format("SELECT * FROM travels WHERE ci_driver = '%s' OR ci_passenger = '%s'", ci, ci);
+        List<Travel> travels = new ArrayList<Travel>();
+        try {
+            Statement selectAll = con.createStatement();
+            ResultSet rs = selectAll.executeQuery(query);
+            
+            while (rs.next()) {
+                Travel t = extract(rs);
+                travels.add(t);
+            }
+            return travels;
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        finally {
+            ConnectionFactory.closeConnection(con);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean insert(Travel t) {
+        Connection con = ConnectionFactory.getConnection();
+        String query = "INSERT INTO travels(ci_driver, ci_passenger, date_ini, matricula_vehicle, seats_available, companions) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            if (ps != null) {
+                ps.setInt(1, t.getCi_driver());
+                ps.setInt(2, t.getCi_passenger());
+                ps.setString(3, t.getDate_ini());
+                ps.setString(4, t.getMatricula_vehicle());
+                ps.setInt(5, t.getSeats_available());
+                ps.setInt(6, t.getCompanions());
+                
+                int i = ps.executeUpdate();
+                
+                if (i == 1) {
+                    return true;
+                }
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        finally {
+            ConnectionFactory.closeConnection(con);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Travel t) {
+        Connection con = ConnectionFactory.getConnection();
+        String query = String.format("UPDATE travels SET date_end WHERE ci_driver = ? AND ci_passenger = ? AND date_ini = ?");
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, t.getCi_driver());
+            ps.setInt(2, t.getCi_passenger());
+            ps.setString(3, t.getDate_ini());
+            
+            int i = ps.executeUpdate();
+            
+            if (i == 1) {
+                return true;
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        finally {
+            ConnectionFactory.closeConnection(con);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Travel t) {
+        return false;
+    }
+    
+}
---
+++ src/main/java/Model/Travel.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author estudiante.fit
+ */
+public class Travel {
+    int ci_driver;
+    int ci_passenger;
+    //POR CAMBIAR: Dates
+    String date_ini;
+    String date_end;
+    String matricula_vehicle;
+    int seats_available;
+    int companions;
+
+    public Travel(int ci_d, int ci_p, String date_i, String date_e, String matricula, int seats, int companions) {
+        this.ci_driver = ci_d;
+        this.ci_passenger = ci_p;
+        this.date_ini = date_i;
+        this.date_end = date_e;
+        this.matricula_vehicle = matricula;
+        this.seats_available = seats;
+        this.companions = companions;
+    }
+    
+    public Travel() {}
+
+    public int getCi_driver() {
+        return ci_driver;
+    }
+
+    public void setCi_driver(int ci_driver) {
+        this.ci_driver = ci_driver;
+    }
+
+    public int getCi_passenger() {
+        return ci_passenger;
+    }
+
+    public void setCi_passenger(int ci_passenger) {
+        this.ci_passenger = ci_passenger;
+    }
+
+    public String getDate_ini() {
+        return date_ini;
+    }
+
+    public void setDate_ini(String date_ini) {
+        this.date_ini = date_ini;
+    }
+
+    public String getDate_end() {
+        return date_end;
+    }
+
+    public void setDate_end(String date_end) {
+        this.date_end = date_end;
+    }
+
+    public String getMatricula_vehicle() {
+        return matricula_vehicle;
+    }
+
+    public void setMatricula_vehicle(String matricula_vehicle) {
+        this.matricula_vehicle = matricula_vehicle;
+    }
+
+    public int getSeats_available() {
+        return seats_available;
+    }
+
+    public void setSeats_available(int seats_available) {
+        this.seats_available = seats_available;
+    }
+
+    public int getCompanions() {
+        return companions;
+    }
+
+    public void setCompanions(int companions) {
+        this.companions = companions;
+    }
+    
+    
+}
